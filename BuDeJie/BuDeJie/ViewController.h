b52426d38b94c644a9d128b1eac67661135dc964
林泉 <linquan@macbook-pro.lan>
2018-09-30 17:41:56 +0800

    初始化一个工程

---
+++ BuDeJie/BuDeJie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BuDeJie
+//
+//  Created by 林泉 on 2018/9/30.
+//  Copyright © 2018年 zwj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BuDeJie/BuDeJie/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  BuDeJie
+//
+//  Created by 林泉 on 2018/9/30.
+//  Copyright © 2018年 zwj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
